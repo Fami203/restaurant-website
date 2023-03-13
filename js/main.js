12db4ff0a3a22f70f69e96c0c6a5c6eaf85c2d62
Fami203 <danilslyusar203@gmail.com>
2023-03-13 20:19:56 +0200

    new page

--- js/main.js
+++ js/main.js
@@ -1,7 +1,46 @@
 $(function () {
-    var mixer = mixitup('.header__menu-chose__wrapper', {
-        load: {
-            filter: '.category-1'
-        }
-    })
+    if(document.querySelector('.header__menu-chose__wrapper')) {
+        var mixer = mixitup('.header__menu-chose__wrapper', {
+            load: {
+                filter: '.category-1'
+            }
+        })
+    }
+
 })
+
+
+const btnmenu = document.querySelector('.header__main-burger')
+const menublock = document.querySelector('.menu-open')
+const showbody = document.querySelector('.showbody')
+const closebtn = document.querySelector('.menu-close')
+
+btnmenu.addEventListener('click', function() {
+    menublock.classList.toggle('none');
+    showbody.classList.toggle('none')
+})
+
+if(closebtn){
+closebtn.addEventListener('click', function() {
+    menublock.classList.toggle('none');
+    showbody.classList.toggle('none')
+})
+}
+
+
+function incrementValue() {
+    let value = parseInt(document.getElementById('counter').value, 10);
+    value = isNaN(value) ? 1 : value;
+    value++;
+    document.getElementById('counter').value = value;
+  }
+  
+  function decrementValue() {
+    let value = parseInt(document.getElementById('counter').value, 10);
+    value = isNaN(value) ? 1 : value;
+    value < 2 ? value = 1 : '';
+    value--;
+    document.getElementById('counter').value = value;
+  }
+
+
